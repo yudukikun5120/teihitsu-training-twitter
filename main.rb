0927c0d7b097cf437be3bf09c57bd3775f3be772
Yuzuki Arai <yudukikun5120@gmail.com>
2022-05-21 04:02:18 +0900

    Change names of keys
    Due to specification change of teihitsu API.

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require 'bundler/setup'
 require 'tweetkit'
 require 'dotenv/load'
 require 'net/https'
+require 'json'
 
 def lootbox_of_item
   case $lootbox
@@ -34,7 +35,7 @@ items = []
 
 until items.count == 4
   item_id = lootbox_of_item
-  p uri = URI.parse("https://teihitsu.deta.dev/items/jyuku-ate/#{item_id}")
+  p uri = URI.parse("https://teihitsu.deta.dev/items/jyuku_ate/#{item_id}")
   p response = Net::HTTP.get_response(uri)
 
   if response.code == '200'
@@ -44,8 +45,10 @@ until items.count == 4
 end
 
 options = []
-items.each { |e| options << e['a'] }
+items.each { |e| options << e['correct_answer'] }
 options.shuffle!
+
+p items
 
 question_item = items[0]
 
@@ -56,16 +59,16 @@ client = Tweetkit::Client.new do |config|
   config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
-p response = client.post_tweet(
-  text: "次の熟字群・当て字の読みを四択より選べ。\n「#{question_item['q']}」〈◆#{question_item['level']}｜Q.#{question_item['item_id']}〉",
+response = client.post_tweet(
+  text: "次の熟字群・当て字の読みを四択より選べ。\n「#{question_item['problem']}」〈◆#{question_item['level']}｜Q.#{question_item['id']}〉",
   poll: {
     options: options,
     duration_minutes: 120
   }
 )
 
-p response = client.post_tweet(
-  text: "答えは「#{question_item['a']}」です。\n\n解説：\n#{question_item['comment']}",
+client.post_tweet(
+  text: "答えは「#{question_item['correct_answer']}」です。\n\n解説：\n#{question_item['note']}",
   reply: {
     in_reply_to_tweet_id: response.response['data']['id']
   }
