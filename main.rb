355cf5a3b821a3bdfe71ee6b1086be5c38bb79fb
Yuzuki Arai <yudukikun5120@gmail.com>
2022-05-24 03:14:09 +0900

    Refactored to improve readability

--- main.rb
+++ main.rb
@@ -5,71 +5,118 @@ require 'tweetkit'
 require 'dotenv/load'
 require 'net/https'
 require 'json'
+require 'pycall/import'
+require 'yaml'
 
-def lootbox_of_item
-  case $lootbox
-  when 0..40
-    rand($level[1])
-  when 40..60
-    rand($level[2])
-  when 60..80
-    rand($level[3])
-  when 80..90
-    rand($level[4])
-  when 90..100
-    rand($level[5])
+# define a problem
+class Problem
+  attr_reader :id, :problem, :level, :correct_answer, :alt_correct_answers, :note
+
+  def initialize(category, id)
+    @category = category
+    @id = id
+    get_content(@category, @id).each_pair do |key, val|
+      instance_variable_set("@#{key}", val)
+    end
+  end
+
+  def get_content(category, id)
+    uri = URI.parse "https://teihitsu.deta.dev/items/#{category}/#{id}"
+    response = Net::HTTP.get_response uri
+    JSON.parse response.body
   end
 end
 
-$level = {
-  1 => 1..459,
-  2 => 460..1362,
-  3 => 1363..1755,
-  4 => 1756..2101,
-  5 => 2102..2184
-}
+# define a quiz
+class Quiz
+  include PyCall::Import
 
-$lootbox = rand(0..100)
+  attr_reader :category, :levels, :ctgr_attr
 
-items = []
+  def initialize(category)
+    @category = category
+    categories = YAML.load_file './categories.yml'
+    @ctgr_attr = categories[category]
 
-until items.count == 4
-  item_id = lootbox_of_item
-  p uri = URI.parse("https://teihitsu.deta.dev/items/jyuku_ate/#{item_id}")
-  p response = Net::HTTP.get_response(uri)
+    @levels = @ctgr_attr.map { |_, attr| attr['level'] }
+    @probabilities = @ctgr_attr.map { |_, attr| attr['probability'] }
+    @ranges = @ctgr_attr.map { |_, attr| attr['range'] }
 
-  if response.code == '200'
-    item = JSON.parse(response.body)
-    items << item
+    @level = get_level
+  end
+
+  def get_level(levels = @levels, probabilities = @probabilities)
+    pyfrom :scipy, import: :stats
+
+    xk = levels
+    pk = probabilities
+    custm = stats.rv_discrete.call({ values: [xk, pk] })
+    custm.rvs
+  end
+
+  def get_problem_id(level = @level, ctgr_attr = @ctgr_attr)
+    ranges = ctgr_attr[level]['ranges']
+    start = ranges['start']
+    end_ = ranges['end']
+    rand start..end_
+  end
+
+  def get_problem(level = @level, category = @category)
+    id = get_problem_id level
+    Problem.new category, id
+  end
+
+  def get_answer_options(level = @level, shuffle: true)
+    answer_options = []
+    until answer_options.count >= 4
+      id = get_problem_id level
+      answer_options << Problem.new(category, id).correct_answer
+    end
+    answer_options.shuffle! if shuffle
+    answer_options
+  end
+
+  def set_client
+    Tweetkit::Client.new do |config|
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_KEY_SECRET']
+      config.access_token = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+  end
+
+  def post_tweets
+    client = set_client
+    problem = get_problem
+    answer_options = get_answer_options
+    tweets = Tweet.new(client, problem, answer_options)
+    res = tweets.first_tweet
+    tweets.second_tweet res
   end
 end
 
-options = []
-items.each { |e| options << e['correct_answer'] }
-options.shuffle!
+Tweet = Struct.new(:client, :problem, :answer_options) do
+  def first_tweet
+    client.post_tweet(
+      text: "次の熟字群・当て字の読みを四択より選べ。\n「#{problem.problem}」〈◆#{problem.level}｜Q.#{problem.id}〉",
+      poll: {
+        options: answer_options,
+        duration_minutes: 120
+      }
+    )
+  end
 
-p items
-
-question_item = items[0]
-
-client = Tweetkit::Client.new do |config|
-  config.consumer_key = ENV['CONSUMER_KEY']
-  config.consumer_secret = ENV['CONSUMER_KEY_SECRET']
-  config.access_token = ENV['ACCESS_TOKEN']
-  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+  def second_tweet(response)
+    client.post_tweet(
+      text: "答えは「#{problem.correct_answer}」です。\n\n解説：\n#{problem.note}",
+      reply: {
+        in_reply_to_tweet_id: response.response['data']['id']
+      }
+    )
+  end
 end
 
-response = client.post_tweet(
-  text: "次の熟字群・当て字の読みを四択より選べ。\n「#{question_item['problem']}」〈◆#{question_item['level']}｜Q.#{question_item['id']}〉",
-  poll: {
-    options: options,
-    duration_minutes: 120
-  }
-)
-
-client.post_tweet(
-  text: "答えは「#{question_item['correct_answer']}」です。\n\n解説：\n#{question_item['note']}",
-  reply: {
-    in_reply_to_tweet_id: response.response['data']['id']
-  }
-)
+if __FILE__ == $PROGRAM_NAME
+  quiz = Quiz.new 'jyuku_ate'
+  quiz.post_tweets
+end
